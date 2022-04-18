61e87602878c7e2a221d5dd37474ae1ac44a2ee2
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-04-18 16:41:18 +0530

    ok submitted

---
+++ Studentdatastructure/src/App.jsx
@@ -0,0 +1,21 @@
+import { AddStudent } from "./components/AddStudent";
+import { ShowStudents } from "./components/ShowStudents";
+
+function App() {
+
+  
+  return (
+    <div className="App">
+
+    
+      <button className="togglebtn"></button>
+      {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
+      <AddStudent/>
+      {/* make sure the table is shown initially, do not show form initially */}
+      <ShowStudents/>
+      {/* make sure to show either of them do not both together */}
+    </div>
+  );
+}
+
+export default App;
